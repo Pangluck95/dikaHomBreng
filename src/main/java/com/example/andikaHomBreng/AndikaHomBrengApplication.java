d4c8997c26cce2f2beab83ac31de9cd547a584b5
Pangestu Kurniawan <pangestuhuang87@gmail.com>
2020-07-20 12:05:37 +0700

    andikaHomBreng

---
+++ src/main/java/com/example/andikaHomBreng/AndikaHomBrengApplication.java
@@ -0,0 +1,13 @@
+package com.example.andikaHomBreng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndikaHomBrengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndikaHomBrengApplication.class, args);
+	}
+
+}
